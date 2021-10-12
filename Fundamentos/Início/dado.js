425f4558f6c5d0eb4588f90b2e341c49321a347a
leorusso2300 <leorusso2607@gmail.com>
2021-10-12 16:07:17 -0300

    Exercicios JS

---
+++ Fundamentos/Início/dado.js
@@ -0,0 +1,13 @@
+var nome = "Caneta";
+var preco  = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto;
+
+console.log(nome);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
+
+
+nome = "Caneta Bic";
+console.log(nome);
---
+++ dado_2.js
@@ -0,0 +1,8 @@
+let preco = 9.99;
+let desconto = 0.4;
+
+console.log(preco * desconto);
+let nome = "Leonardo ";
+let sobrenome = "Gabriel ";
+let idade = 22;
+console.log("nome: " + nome + "Sobrenome: " + sobrenome + "Idade: " + idade);
