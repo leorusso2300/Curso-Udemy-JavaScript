19e21df71b2b502707429c47391fd185216bc782
leorusso2300 <leorusso2607@gmail.com>
2021-10-12 10:36:13 -0300

    Primeiros exercicios JS

---
+++ Fundamentos/Início/comentario.js
@@ -0,0 +1,8 @@
+console.log('Bom dia!')// Exemplo de comentário 1
+
+/*
+Esse é outro exemplo de comentário
+*/
+
+console.log('Boa Tarde!')// Exemplo de comentário 2
+console.log('Boa noite!')// Exemplo de comentário 3
